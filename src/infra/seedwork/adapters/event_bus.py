145b6f12db7d013ebfe983f81b386bf171471527
fedor <fedorlastv@gmail.com>
2025-11-14 07:47:12 +0300

    feat(event_bus): add logger

--- src/infra/seedwork/adapters/event_bus.py
+++ src/infra/seedwork/adapters/event_bus.py
@@ -3,6 +3,7 @@ import logging
 
 from domain.seedwork.event import DomainEvent
 from infra.seedwork.adapters.inbox_outbox.interfaces.outbox import Outbox
+from infra.seedwork.adapters.inbox_outbox.exceptions.outbox import OutboxException
 
 
 logger = logging.getLogger(__name__)
@@ -13,6 +14,17 @@ class EventBusImpl:
     _outbox: Outbox
 
     async def publish(self, event: DomainEvent) -> None:
-        await self._outbox.add(event=event)
+        try:
+            await self._outbox.add(event=event)
 
-        logger.debug("Event sent to outbox!", extra={"event_name": event.__class__.__name__})
+        except OutboxException as err:
+            logger.error(
+                "Event bus: Failed to send event to outbox!",
+                err,
+                extra={"event_name": event.__class__.__name__},
+            )
+
+            raise err
+
+        else:
+            logger.debug("Event sent to outbox!", extra={"event_name": event.__class__.__name__})
